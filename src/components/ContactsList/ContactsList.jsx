eec3ba38db4fca1355c1dfa1c7ee83d85d3a5064
Silich Alexander <sili41995@gmail.com>
2023-09-30 16:01:52 +0300

    update

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -3,19 +3,22 @@ import { Container, List } from './ContactsList.styled';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectContacts } from 'redux/contacts/selectors';
+import EmptyListMessage from 'components/EmptyListMessage/EmptyListMessage';
 
 const ContactsList = () => {
   const contacts = useSelector(selectContacts);
 
   return (
     <Container>
-      {!!contacts.length && (
+      {contacts.length ? (
         <List
           contentContainerStyle={{ gap: 8 }}
           data={contacts}
           renderItem={({ item }) => <ContactsListItem contact={item} />}
           keyExtractor={(item) => `${item.id}`}
         />
+      ) : (
+        <EmptyListMessage />
       )}
     </Container>
   );
--- src/screens/ContactsScreen.jsx
+++ src/screens/ContactsScreen.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import ContactsList from 'components/ContactsList/ContactsList';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
+import { useDispatch } from 'react-redux';
 
 const ContactsScreen = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const promise = dispatch(fetchContacts());
+
+    return () => {
+      promise.abort();
+    };
+  }, [dispatch]);
+
   return <ContactsList />;
 };
 
